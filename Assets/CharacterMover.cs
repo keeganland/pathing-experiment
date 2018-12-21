5a7fe6800a00e93bc334ae562638aa94207bb2b2
Keegan-NewDesktop <keeganland@gmail.com>
2018-12-21 02:15:25 -0800

    Collisions are respected. Basic player character

---
+++ Assets/CharacterMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour {
+
+    public float moveSpeedf = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveCharacter(float x, float y)
+    {
+        Vector3 dirVector = new Vector3(x, y, 0).normalized * moveSpeedf;
+        GetComponent<Rigidbody2D>().MovePosition(transform.position + dirVector * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    private CharacterMover playerMover;
+    [SerializeField]
+    private float xMovementf = 0.0f, yMovementf = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerMover = GetComponent<CharacterMover>();
+    }
+
+
+
+    // Update is for scanning for inputs
+    void Update()
+    {
+
+        xMovementf = Input.GetAxisRaw("Horizontal");
+        yMovementf = Input.GetAxisRaw("Vertical");
+
+    }
+
+    // FixedUpdate is for passing inputs along to the controller script. 
+    private void FixedUpdate()
+    {
+
+        playerMover.MoveCharacter(xMovementf, yMovementf);
+    }
+
+
+}
